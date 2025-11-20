429fbf5fefa1cfe40677a943b1559ed4986531cf
Joe-Takayama <jgaoshan549@gmail.com>
2025-11-20 20:46:55 +0900

    ページ遷移できるようになりました

--- spotproject/spotapp/views.py
+++ spotproject/spotapp/views.py
@@ -5,4 +5,10 @@ class IndexView(View):
     def get(self, request):
         return render(request, 'spotapp/index.html')
     
+class TekitoView(View):
+    def get(self, request):
+        return render(request, 'spotapp/tekito.html')
+    
+    
 index = IndexView.as_view()
+tekito = TekitoView.as_view()
