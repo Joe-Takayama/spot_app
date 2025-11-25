4b7c1f962fb59c662ae813131ec05d791630375d
Joe-Takayama <jgaoshan549@gmail.com>
2025-11-25 10:54:40 +0900

    ログイン用のフォルダを追加

--- spotproject/spotapp_admin/views.py
+++ spotproject/spotapp_admin/views.py
@@ -20,13 +20,13 @@ class RegistselectView(View):
 class LoginView(View):
     def get(self, request):
         form = StaffForm()
-        return render(request, 'spotapp_admin/login_form.html', {'form': form})
+        return render(request, 'accounts/login_form.html', {'form': form})
     
     def post(self, request):
         form = StaffForm(request.POST)
 
         if not form.is_valid():
-            return render(request, 'spotapp_admin/login_form.html', {'form': form})
+            return render(request, 'accounts/login_form.html', {'form': form})
         
         name = form.cleaned_data['name']
         password = form.cleaned_data['password']
@@ -36,18 +36,18 @@ class LoginView(View):
             staff = Staff.objects.get(name=name)
         except Staff.DoesNotExist:
             messages.error(request, '職員名が正しくありません')
-            return render(request, 'spotapp_admin/login_form.html', {'form': form})
+            return render(request, 'accounts/login_form.html', {'form': form})
         
         # パスワード照合
         if check_password(password, staff.password):
             # ログイン成功
             request.session['staff_id'] = str(staff.staff_id)
 
-            return render(request, 'spotapp_admin/login_success.html', {'statt': staff})
+            return render(request, 'accounts/login_success.html', {'statt': staff})
         
         # パスワード不一致
         messages.error(request, 'パスワードが違います')
-        return render(request, 'spotapp_admin/login_form.html', {'form': form})
+        return render(request, 'accounts/login_form.html', {'form': form})
 
 
     
