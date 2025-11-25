b56b10c5fb62bae044ad79b59ad7f4cb5f977246
海斗 <onuma.kaito1009@icloud.com>
2025-11-25 09:42:44 +0900

    viesのところに追加でかいたよ

--- spotproject/spotapp_admin/views.py
+++ spotproject/spotapp_admin/views.py
@@ -4,7 +4,7 @@ from django.views import View
 class IndexView(View):
     def get(self, request):
         return render(request, 'spotapp_admin/index.html')
-    
+#登録選択画面
 class RegistselectView(View):
     def get(self,request):
         return render(request,'spotapp_admin/Registrationselection.html')
