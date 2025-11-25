a7142920d23fe5316477c0236d1b5309d80aa31c
Joe-Takayama <jgaoshan549@gmail.com>
2025-11-25 10:48:42 +0900

    管理者のログインができるように作りました

--- spotproject/spotapp_admin/views.py
+++ spotproject/spotapp_admin/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views import View
 from .forms import StaffForm
+from .models import Staff
+from django.contrib import messages
+from django.contrib.auth.hashers import check_password
 
 
 #ホーム画面
@@ -19,7 +22,32 @@ class LoginView(View):
         form = StaffForm()
         return render(request, 'spotapp_admin/login_form.html', {'form': form})
     
+    def post(self, request):
+        form = StaffForm(request.POST)
 
+        if not form.is_valid():
+            return render(request, 'spotapp_admin/login_form.html', {'form': form})
+        
+        name = form.cleaned_data['name']
+        password = form.cleaned_data['password']
+
+        # 名前で検索
+        try:
+            staff = Staff.objects.get(name=name)
+        except Staff.DoesNotExist:
+            messages.error(request, '職員名が正しくありません')
+            return render(request, 'spotapp_admin/login_form.html', {'form': form})
+        
+        # パスワード照合
+        if check_password(password, staff.password):
+            # ログイン成功
+            request.session['staff_id'] = str(staff.staff_id)
+
+            return render(request, 'spotapp_admin/login_success.html', {'statt': staff})
+        
+        # パスワード不一致
+        messages.error(request, 'パスワードが違います')
+        return render(request, 'spotapp_admin/login_form.html', {'form': form})
 
 
     
