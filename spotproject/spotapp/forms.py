9a18eb67257e6ced61d1d513c47ca2368fa58ba1
takano7293 <yuki067293@gmail.com>
2025-11-26 18:38:26 +0900

    新規登録の機能が完成した

--- spotproject/spotapp/forms.py
+++ spotproject/spotapp/forms.py
@@ -3,14 +3,22 @@ from django import forms
 from django.contrib.auth.models import User
 
 # 新規登録用フォーム
-class UserForm(forms.ModelForm):
+class SignupForm(forms.ModelForm):
+    password = forms.CharField(
+        label="パスワード",
+        widget=forms.PasswordInput
+    )
+
     class Meta:
         model = User
-        fields = ["username", "password"]
-        widgets = {
-            "password": forms.PasswordInput(),
-        }
+        fields = ["username", "email", "password"]
 
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])  # パスワード暗号化
+        if commit:
+            user.save()
+        return user
 
 #プロフィール編集用フォーム
 class ProfileEditForm(forms.ModelForm):
--- spotproject/spotapp/views.py
+++ spotproject/spotapp/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import update_session_auth_hash
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .forms import ProfileEditForm, PasswordChangeOnlyForm
+from .forms import ProfileEditForm, PasswordChangeOnlyForm, SignupForm
 
 class IndexView(View):
     def get(self, request):
@@ -14,16 +13,22 @@ class IndexView(View):
 #  新規登録ビュー   
 class SignupView(View):
     def get(self, request):
-        form = UserCreationForm()
+        form = SignupForm()
         return render(request, "spotapp/signup.html", {"form": form})
 
     def post(self, request):
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
+
         if form.is_valid():
-            user = form.save()
-            login(request, user)
+            user = form.save(commit=False)   # まず user インスタンス作成（DB にはまだ保存しない）
+            user.set_password(form.cleaned_data["password"])  # パスワードをハッシュ化
+            user.save()  # ← DB に保存！（ここが本物の save）
+
+            login(request, user)  # 自動ログイン
             return redirect("spotapp:signup_complete")
+
         return render(request, "spotapp/signup.html", {"form": form})
+
     
 # 新規登録完了ビュー
 class SignupCompleteView(View):
