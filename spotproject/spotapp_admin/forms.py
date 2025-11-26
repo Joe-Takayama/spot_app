1f0db646280577c1c34e274db0c29bcd7267150a
海斗 <onuma.kaito1009@icloud.com>
2025-11-26 12:22:30 +0900

    観光地登録関連終

--- spotproject/spotapp_admin/forms.py
+++ spotproject/spotapp_admin/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm, TextInput, PasswordInput, Textarea
-from .models import Staff, Events, Photo
+from .models import Staff, Events, Photo, Spot
 from django import forms
 
 #職員ログインフォーム
@@ -45,3 +45,25 @@ class PhotoForm(ModelForm):
         model = Photo
         fields = ['image']
         labels = {'image': '写真を登録してください'}
+
+class SpotCreateForm(ModelForm):
+    class Meta:
+        model = Spot
+        fields = ['spot_name', 'spot-address', 'spot-time', 'Admission fee', 'etailed information']
+        widgets = {
+            'spot_name': TextInput(attrs={
+                'placeholder': '観光地名称を入力してください'
+            }),
+            'spot-address': TextInput(attrs={
+                'placeholder': '住所を入力してください'
+            }),
+            'spot-time': TextInput(attrs={
+                'placeholder': '営業時間を入力してください'
+            }),
+            'Admission fee': Textarea(attrs={
+                'placeholder': '入場料を入力してください'
+            }),
+            'etailed information': TextInput(attrs={
+                'placeholder': '詳細情報を入力してください'
+            }),
+        }
