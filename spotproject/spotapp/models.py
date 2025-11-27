a133b8be2d10dda7c7825e336f673a217259a07f
kbtit700-svgr <jupiterbot6@gmail.com>
2025-11-27 10:37:38 +0900

    models.pyにevent追加した

--- spotproject/spotapp/models.py
+++ spotproject/spotapp/models.py
@@ -107,3 +107,21 @@ class Review(models.Model):
 
     def __str__(self):
         return f"{self.spot} - {self.rating}点"
+    
+    
+# イベント
+import uuid
+from django.db import models
+
+class Event(models.Model):
+    event_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200, verbose_name="イベント名")
+    date = models.CharField(max_length=100, verbose_name="開催日")
+    location = models.CharField(max_length=200, verbose_name="開催場所")
+    url = models.URLField(blank=True, null=True, verbose_name="外部リンク")
+    description = models.TextField(blank=True, null=True, verbose_name="説明")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="登録日時")
+
+    def __str__(self):
+        return self.title
+
