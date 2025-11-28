b2608c46c50b4f73d5eb1544100285c37ff56822
Joe-Takayama <jgaoshan549@gmail.com>
2025-11-28 14:02:29 +0900

    DB新規作成できました

--- spotproject/spotproject/settings.py
+++ spotproject/spotproject/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -83,11 +84,10 @@ WSGI_APPLICATION = 'spotproject.wsgi.application'
 
 #database_config = dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
 
+DATABASE_URL = os.getenv('DATABASE_URL')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
 }
 
 
