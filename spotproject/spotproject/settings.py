195c1e0d25ed03503392655be24fb3e48532972f
Joe-Takayama <jgaoshan549@gmail.com>
2025-11-28 09:48:01 +0900

    データベースを基に戻した

--- spotproject/spotproject/settings.py
+++ spotproject/spotproject/settings.py
@@ -79,13 +79,18 @@ WSGI_APPLICATION = 'spotproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+#DATABASE_URL = os.getenv('DATABASE_URL')
+
+#database_config = dj_database_url.parse(DATABASE_URL, conn_max_age=600, ssl_require=True)
 
 DATABASES = {
-    'default': dj_database_url.parse(DATABASE_URL, conn_max_age=600)
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
-DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
+
 
 
 # Password validation
