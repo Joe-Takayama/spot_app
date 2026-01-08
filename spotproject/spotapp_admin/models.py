3c2da24855582bce5d08df1cf9f754d37cfa5601
futa <futa17728bulu@icloud.com>
2026-01-08 12:32:03 +0900

    テーブル変更

--- spotproject/spotapp_admin/models.py
+++ spotproject/spotapp_admin/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from spotapp.models import Spot, Events
 
 # 職員（管理者）テーブル
 class Staff(models.Model):
@@ -13,72 +14,72 @@ class Staff(models.Model):
 
 
 # 地区別テーブル
-class District(models.Model):
+# class District(models.Model):
     # 地区ID
-    district_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # district_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # 地区名称
-    district_name = models.CharField(max_length=100, verbose_name="地区名称")
+    # district_name = models.CharField(max_length=100, verbose_name="地区名称")
 
-    def __str__(self):
-        return self.district_name
+    # def __str__(self):
+        # return self.district_name
 
 
 # カテゴリテーブル
-class Category(models.Model):
+# class Category(models.Model):
     # カテゴリID
-    category_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    # category_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     # カテゴリ名称
-    category_name = models.CharField(max_length=100, verbose_name="カテゴリ名称")
+    # category_name = models.CharField(max_length=100, verbose_name="カテゴリ名称")
 
-    def __str__(self):
-        return self.category_name
+    # def __str__(self):
+        # return self.category_name
 
 
 # 観光地テーブル
-class Spot(models.Model):
+#class Spot(models.Model):
     # 観光地ID
-    spot_id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
+    # spot_id = models.UUIDField(primary_key=True,default=uuid.uuid4, editable=False)
     # 観光地名称
-    spot_name = models.CharField(max_length=100, verbose_name="観光地名称")
+    # spot_name = models.CharField(max_length=100, verbose_name="観光地名称")
     # 住所
-    address = models.CharField(max_length=200, verbose_name="住所")
+    # address = models.CharField(max_length=200, verbose_name="住所")
     # カテゴリ
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    # category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     # 説明
-    explanation = models.TextField(max_length=2000, verbose_name="説明")
+    # explanation = models.TextField(max_length=2000, verbose_name="説明")
     # 営業時間
-    business_hours = models.CharField(max_length=100, verbose_name="営業時間")
+    # business_hours = models.CharField(max_length=100, verbose_name="営業時間")
     # 定休日
-    regular_holiday = models.CharField(max_length=100, verbose_name="定休日")
+    # regular_holiday = models.CharField(max_length=100, verbose_name="定休日")
     # 登録日
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="登録日時")
+    # created_at = models.DateTimeField(auto_now_add=True, verbose_name="登録日時")
     # 地区
-    district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
+    # district = models.ForeignKey(District, on_delete=models.SET_NULL, null=True)
 
-    def __str__(self):
-        return self.spot_name
+    # def __str__(self):
+        # return self.spot_name
     
 
-class Events(models.Model):
-    # イベントID
-    event_id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, null=False, blank=False, editable=False)
-    # イベント名称
-    event_name = models.CharField(max_length=255, verbose_name="イベント名称")
-    # 観光地ID
-    spot_id = models.ForeignKey(Spot, on_delete=models.CASCADE, null=True, blank=True)
-    # 開催日
-    event_date = models.DateField(verbose_name="開催日")
-    # 会場
-    venue = models.CharField(max_length=255, verbose_name="会場")
-    # 詳細
-    details = models.TextField(max_length=2000, verbose_name="詳細情報")
-    # 主催者
-    organizer = models.CharField(max_length=255, verbose_name="主催者")
-    # 登録日
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="登録日時")
+# class Events(models.Model):
+#     # イベントID
+#     event_id = models.UUIDField(primary_key=True, default=uuid.uuid4, unique=True, null=False, blank=False, editable=False)
+#     # イベント名称
+#     event_name = models.CharField(max_length=255, verbose_name="イベント名称")
+#     # 観光地ID
+#     spot_id = models.ForeignKey(Spot, on_delete=models.CASCADE, null=True, blank=True)
+#     # 開催日
+#     event_date = models.DateField(verbose_name="開催日")
+#     # 会場
+#     venue = models.CharField(max_length=255, verbose_name="会場")
+#     # 詳細
+#     details = models.TextField(max_length=2000, verbose_name="詳細情報")
+#     # 主催者
+#     organizer = models.CharField(max_length=255, verbose_name="主催者")
+#     # 登録日
+#     created_at = models.DateTimeField(auto_now_add=True, verbose_name="登録日時")
 
-    def __str__(self):
-        return self.event_name
+#     def __str__(self):
+#         return self.event_name
     
 
 class Photo(models.Model):
@@ -89,8 +90,8 @@ class Photo(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name="アップロード日時")
 
     # SpotテーブルとEventsテーブルの両方に紐づけ
-    spot = models.ForeignKey(Spot, on_delete=models.CASCADE, null=True, blank=True, related_name='photos')
-    event = models.ForeignKey(Events, on_delete=models.CASCADE, null=True, blank=True, related_name='photos')
+    spot = models.ForeignKey(Spot, on_delete=models.CASCADE, null=True, blank=True, related_name='spot_photos')
+    event = models.ForeignKey(Events, on_delete=models.CASCADE, null=True, blank=True, related_name='event_photos')
 
     def __str__(self):
         if self.spot:
