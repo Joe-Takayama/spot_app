d8356fa14082f7c86d00e4562a5c165b97e28fba
Joe-Takayama <jgaoshan549@gmail.com>
2026-01-20 11:41:24 +0900

    お知らせの表示が直った（管理者）

--- spotproject/spotapp/context_processors.py
+++ spotproject/spotapp/context_processors.py
@@ -1,9 +1,7 @@
 from .models import Category, District
-from spotapp_admin.models import Osirase
 
 def common_nav_data(request):
     return {
         "categories": Category.objects.all(),
         "districts": District.objects.all(),
-        "osirase_list": Osirase.objects.all(),
     }
