0307d8343738f6cf6aa6cec6b5279934b0e766e9
Joe-Takayama <jgaoshan549@gmail.com>
2026-01-29 14:58:01 +0900

    セッティング修正

--- spotproject/settings.py
+++ spotproject/settings.py
@@ -135,12 +135,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = (
-    [
-        os.path.join(BASE_DIR,"spotapp" , "static"), 
-        os.path.join(BASE_DIR,"spotapp_admin", "static"),
-    ]
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "spotapp", "static"),
+    os.path.join(BASE_DIR, "spotapp_admin", "static"),
+]
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
