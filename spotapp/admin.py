0c7c130ed2d85d3b568349b2a56dd5c8c49d3a61
futa <futa17728bulu@icloud.com>
2026-02-06 14:55:30 +0900

    db見るために色々

--- spotapp/admin.py
+++ spotapp/admin.py
@@ -1,9 +1,18 @@
-from django.contrib import admin
-from .models import Spot
-from .models import Events
-from .models import Profile
 
+from django.contrib import admin
+
+
+from .models import Spot,Events,Profile,Review,Favorite,Category,District
+from spotapp_admin.models import  Osirase
 
 admin.site.register(Spot)
 admin.site.register(Events)
 admin.site.register(Profile)
+admin.site.register(Review)
+admin.site.register(Favorite)
+admin.site.register(Category)
+admin.site.register(District)
+admin.site.register(Osirase)
+
+
+
